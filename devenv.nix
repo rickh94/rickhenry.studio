97f269cf98114b26394b9ada986dfcd1fd846fb7
Rick Henry <rickhenry@rickhenry.dev>
2023-09-07 14:19:29 -0400

    install wordpress
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
 
@@ -9,14 +9,64 @@
     pkgs.nodejs
     pkgs.nodePackages.tailwindcss
     pkgs.nodePackages.pnpm
+    pkgs.php82
+    pkgs.wp-cli
+    pkgs.wordpress
   ];
 
   languages = {
     javascript = {
       enable = true;
+      package = pkgs.nodejs;
+    };
+    php = {
+      enable = true;
+      fpm.pools.web = {
+        settings = {
+          "clear_env" = "no";
+          "pm" = "dynamic";
+          "pm.max_children" = 10;
+          "pm.start_servers" = 2;
+          "pm.min_spare_servers" = 1;
+          "pm.max_spare_servers" = 3;
+        };
+        phpOptions = ''
+          upload_max_filesize = 32M
+          post_max_size = 64M
+          memory_limit = 128M
+        '';
+      };
     };
   };
 
+  certificates = [
+    "studio.localhost"
+  ];
 
-  scripts.view.exec = "microserver --no-spa design";
+  services.caddy = {
+    enable = true;
+    virtualHosts."studio.localhost" = {
+      extraConfig = ''
+        root * public
+        php_fastcgi unix/${config.languages.php.fpm.pools.web.socket} 
+        file_server
+      '';
+    };
+  };
+
+  services.mysql = {
+    enable = true;
+    initialDatabases = [{ name = "studio"; }];
+    ensureUsers = [{
+      name = "studio";
+      password = "studio";
+      ensurePermissions = { "studio.*" = "ALL PRIVILEGES"; };
+    }];
+  };
+
+  env.DB_NAME = "studio";
+  env.DB_USER = "studio";
+  env.DB_PASSWORD = "studio";
+
+
 }
